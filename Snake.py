c3267b0b6c229f3f701da366416fd765f54ace9f
JacobHoffstedt <jacob.hoffstedt@hotmail.com>
2025-03-26 00:49:24 +0100

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,8 +1,3 @@
-#Snake 
-#64x64 kanske vita plupp 1x8 först åker framåt, kan styras med upp höger vänster ned
-#Behöver något som trackar dess position, om den träffar äpplet ökar den med +1 i baken och hastigheten ökar
-#Behöver något som håller koll på vad som är del av ormen så att man vet ifall den träffar sig själv. 
-
 
 import pygame
 import sys
