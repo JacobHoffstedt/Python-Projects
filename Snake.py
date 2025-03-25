f1508d338e919b0f2e516996bad167608398a915
JacobHoffstedt <jacob.hoffstedt@hotmail.com>
2025-03-25 23:21:31 +0100

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -18,13 +18,23 @@ pygame.display.set_caption("Snake Game") #Setting name in title bar
 clock = pygame.time.Clock() #Sets up the framerate of the game  
 #Initiates the black box (the game screen) it runs until its crossed manually. 
 #Snake
-block_size = 20
-snake_color = (0, 255, 0)
-snake = [(300, 200), (280, 200), (260, 200)]
+block_size = 20 #Size of each piece of the snake
+snake_color = (0, 255, 0) #Green color
+snake = [(300, 200), (280, 200), (260, 200)] #Initial snake as x and y coordinates. 
 
-def draw_snake(snake):
-    for segment in snake:
-        pygame.draw.rect(screen, snake_color, (segment[0], segment[1], block_size, block_size))
+
+#Drawing the snake
+#for block in snake: Loops over each of the coordinates in the snake variable.
+#pygame.draw.rect: Draws a rectangle using ()
+#screen is the created earlier
+#snake_color is defined earlier
+#block[0] and block[1] refers to the x and y values of the snake, calls the coordinates in the snake variable created.
+#block_size is the width and height of the snake.
+def draw_snake(snake): 
+    for block in snake:
+        pygame.draw.rect(screen, snake_color, (block[0], block[1], block_size, block_size)) #pygame.draw.rect(SURFACE, COLOR, (x, y, width, height))
+
+
 
 
 running = True
